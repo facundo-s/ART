33d309b6477490bbd2ac5b82bb0cf132e20d317f
Jacob Posner <jashaszun123@gmail.com>
2017-04-21 12:14:40 -0700

    Adding google api services for transportation.

--- app/src/main/java/com/example/art/art/Transportation.java
+++ app/src/main/java/com/example/art/art/Transportation.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Transportation extends AppCompatActivity {
 
@@ -13,6 +14,22 @@ public class Transportation extends AppCompatActivity {
         setContentView(R.layout.activity_transportation);
     }
 
+    public void from_home_clicked(View view) {
+        EditText from_input = (EditText)findViewById(R.id.from_location_input);
+        from_input.setText("Southside");
+    }
+    public void from_work_clicked(View view) {
+        EditText from_input = (EditText)findViewById(R.id.from_location_input);
+        from_input.setText("Jacobs Hall");
+    }
+    public void to_home_clicked(View view) {
+        EditText to_input = (EditText)findViewById(R.id.to_location_input);
+        to_input.setText("Southside");
+    }
+    public void to_work_clicked(View view) {
+        EditText to_input = (EditText)findViewById(R.id.to_location_input);
+        to_input.setText("Jacobs Hall");
+    }
     public void load_done(View view) {
         Intent result = new Intent();
         result.putExtra("log_data", "Requested transportation.");
